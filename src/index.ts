40a2c9a8fff195ae02f256f092470f8631f68d63
dotranminhchu <dotranminhchu@gmail.com>
2023-04-20 03:48:53 +0700

    [REMOVE] : Gen pem

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 
 import { App } from "./app";
 
-import { cryptoService, scheduleService } from "./services";
+import { scheduleService } from "./services";
 import { serverSocket } from "./services/socketIO";
 
 const app = new App();
 serverSocket.init(app.app);
 scheduleService.run();
 app.init()
-
-
-cryptoService.generateKeyPairSync('pem', true, 'identity')
