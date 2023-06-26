ab22e0217a70378a3cf5aa397dd80992438e69e7
Do Tran Minh Chu <dotranminhchu@gmail.com>
2023-06-26 22:25:18 +0700

    [FEAT] : Get provider by user id

--- src/controllers/system/provider.controller.ts
+++ src/controllers/system/provider.controller.ts
@@ -1,6 +1,6 @@
 import { providerRepository } from "@/repositories";
 import { BaseController, Request, Response } from "../base/base.controller";
-import { providerService } from "@/services";
+import { errorService, providerService } from "@/services";
 import { ProviderService } from "@/services/api/v1/provider.service";
 
 export class providerController extends BaseController {
@@ -8,11 +8,32 @@ export class providerController extends BaseController {
         super()
         this.service = providerService
         this.path = 'provider'
+        this.router.get("/get-provider-by-user-id/:id", this.systemsMiddlewares(), this.route(this.getProviderByUserId));
         this.router.post("/get-list-provider-by-user-ids", this.systemsMiddlewares(), this.route(this.getListProviderByUserIds));
 
 
     }
-    service: ProviderService
+    service: ProviderService;
+
+    async getProviderByUserId(req: Request, res: Response) {
+        const { id } = req.params;
+        if (!id) {
+            throw errorService.router.badRequest
+        }
+        const result = await providerRepository.findOne({
+            where: {
+                userId: id
+            },
+            select: {
+                id: true,
+                userId: true,
+                name: true,
+                slug: true,
+                avatarUrl: true
+            }
+        })
+        this.onSuccess(res, result)
+    }
     async getListProviderByUserIds(req: Request, res: Response) {
         const { ids } = req.body;
         const result = await providerRepository.findAndCountAll({
