cb54a7b2a2330d9a49466e76335bfd9db00381a9
Do Tran Minh Chu <dotranminhchu@gmail.com>
2023-07-22 01:15:24 +0700

    [CONFIG] : Version

--- src/repositories/base/basePrisma.repository.ts
+++ src/repositories/base/basePrisma.repository.ts
@@ -1,13 +1,15 @@
 import prisma from "@/models/base.prisma";
 import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
 type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
 export type PrismaTransation = Omit<
   PrismaClient<
     Prisma.PrismaClientOptions,
     never,
-    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+    DefaultArgs
+
   >,
-  "$connect" | "$disconnect" | "$on" | "$transaction" | "$use"
+  "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends"
 >;
 export class BasePrismaRepository {
   constructor() {
