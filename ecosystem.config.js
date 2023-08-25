8c297581ace1718612f1b080509af42ca49f454b
dotranminhchu <dotranminhchu@gmail.com>
2023-08-25 14:35:21 +0700

    [CONFIG]

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,22 @@ const path = require("path");
 const directoryName = path.basename(__dirname);
 
 module.exports = {
-  apps: [{
-    name: directoryName,
-    script: './build/index.js',
-    node_args: '-r ts-node/register -r tsconfig-paths/register',
-    watch: false,
-    restart_delay: 10000,
-    wait_ready: true,
-    env: {
-      NODE_ENV: 'production',
-    },
-    env_production: {
-      NODE_ENV: 'production',
-    },
-  }],
-
+    apps: [
+        {
+            name: directoryName,
+            script: "./build/index.js",
+            node_args: "-r ts-node/register -r tsconfig-paths/register",
+            watch: false,
+            restart_delay: 10000,
+            wait_ready: true,
+            env: {
+                NODE_ENV: "production",
+                PREFIX_PATH: "booking-service",
+            },
+            env_production: {
+                NODE_ENV: "production",
+                PREFIX_PATH: "booking-service",
+            },
+        },
+    ],
 };
